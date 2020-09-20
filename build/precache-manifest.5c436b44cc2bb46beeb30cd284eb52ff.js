9faa29c62cdd11ce8d9f54a34cf161f8dfbbff60
Darshan <darshandhokajain@gmail.com>
2020-09-20 14:20:29 +0530

    deployment

---
+++ build/precache-manifest.5c436b44cc2bb46beeb30cd284eb52ff.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4720b566a34e0226a48e0e309120cffe",
+    "url": "/darshan-jain/apada/index.html"
+  },
+  {
+    "revision": "7039c57ca596912ff0b2",
+    "url": "/darshan-jain/apada/static/css/2.2fedd682.chunk.css"
+  },
+  {
+    "revision": "0d65954dfb976bf859e6",
+    "url": "/darshan-jain/apada/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7039c57ca596912ff0b2",
+    "url": "/darshan-jain/apada/static/js/2.5a75be83.chunk.js"
+  },
+  {
+    "revision": "6d34f815620849a5469c05a9d36c6875",
+    "url": "/darshan-jain/apada/static/js/2.5a75be83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d65954dfb976bf859e6",
+    "url": "/darshan-jain/apada/static/js/main.4f162c1c.chunk.js"
+  },
+  {
+    "revision": "d12fc47563126e995509",
+    "url": "/darshan-jain/apada/static/js/runtime-main.e179e18d.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/darshan-jain/apada/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/darshan-jain/apada/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/darshan-jain/apada/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/darshan-jain/apada/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/darshan-jain/apada/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/darshan-jain/apada/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/darshan-jain/apada/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/darshan-jain/apada/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/darshan-jain/apada/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/darshan-jain/apada/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/darshan-jain/apada/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/darshan-jain/apada/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/darshan-jain/apada/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/darshan-jain/apada/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/darshan-jain/apada/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/darshan-jain/apada/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/darshan-jain/apada/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
